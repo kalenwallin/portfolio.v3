eb872f3374eb7152080e3bf3efafa9c8b06c2e6e
Kalen <31481852+kalenwallin@users.noreply.github.com>
2025-05-11 14:51:24 -0600

    Disable search

--- lib/config.ts
+++ lib/config.ts
@@ -109,7 +109,7 @@ export const navigationLinks: Array<NavigationLink | null> = getSiteConfig(
 )
 
 // Optional site search
-export const isSearchEnabled: boolean = getSiteConfig('isSearchEnabled', true)
+export const isSearchEnabled: boolean = getSiteConfig('isSearchEnabled', false)
 
 // ----------------------------------------------------------------------------
 
