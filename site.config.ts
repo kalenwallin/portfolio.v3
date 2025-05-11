965ce53215607b29df75cbe9977ff3792b533aff
Kalen <31481852+kalenwallin@users.noreply.github.com>
2025-05-10 23:28:15 -0600

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -11,7 +11,7 @@ export default siteConfig({
   // basic site info (required)
   name: 'Westside Church of Christ',
   domain: 'imperial.church',
-  author: 'Westside Church of Christ',
+  author: 'Imperial, Nebraska',
 
   // open graph metadata (optional)
   description: "Proclaiming Jesus Christ as Lord (Romans 10:9). Watch past sermons, grow in the Word, and join us in living out the Great Commission at the Westside Church of Christ in Imperial, Nebraska.",
