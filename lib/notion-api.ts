f1c8d8cef3d0f18d0e92979aeb39b2fc7cfcfb2c
Kalen <31481852+kalenwallin@users.noreply.github.com>
2025-10-12 17:30:34 -0500

    Revert "Update notion-api.ts"
    This reverts commit dcc423092c0b3030696b131637e1a0735b35c099.

--- lib/notion-api.ts
+++ lib/notion-api.ts
@@ -1,7 +1,5 @@
 import { NotionAPI } from 'packages/notion-client/src/notion-api'
 
 export const notion = new NotionAPI({
-  apiBaseUrl: process.env.NOTION_API_BASE_URL,
-  activeUser: process.env.NOTION_ACTIVE_USER,
-  authToken: process.env.NOTION_TOKEN_V2
+  apiBaseUrl: process.env.NOTION_API_BASE_URL
 })
