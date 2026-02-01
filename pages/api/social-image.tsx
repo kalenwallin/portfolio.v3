7b8f043eeaf694de2fd8cbca121cc5429044f7e9
Kalen <31481852+kalenwallin@users.noreply.github.com>
2026-02-01 08:49:47 -0600

    Remove the white rectangle from the social image

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -85,70 +85,21 @@ export default async function OGImage(
           width: 900,
           height: 465,
           display: 'flex',
-          flexDirection: 'column',
-          border: '16px solid rgba(0,0,0,0.3)',
-          borderRadius: 8,
-          zIndex: '1'
+          backgroundColor: '#1F2027'
         }}
       >
-        <div
-          style={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-around',
-            backgroundColor: '#fff',
-            padding: 24,
-            alignItems: 'center',
-            textAlign: 'center'
-          }}
-        >
-          {pageInfo.detail && (
-            <div style={{ fontSize: 32, opacity: 0 }}>{pageInfo.detail}</div>
-          )}
-
-          <div
-            style={{
-              fontSize: 70,
-              fontWeight: 700,
-              fontFamily: 'Inter'
-            }}
-          >
-            {pageInfo.title}
-          </div>
-
-          {pageInfo.detail && (
-            <div style={{ fontSize: 32, opacity: 0.6 }}>{pageInfo.detail}</div>
-          )}
-        </div>
-      </div>
-
-      {pageInfo.authorImage && (
-        <div
-          style={{
-            position: 'absolute',
-            top: 47,
-            left: 104,
-            height: 128,
-            width: 128,
-            display: 'flex',
-            borderRadius: '50%',
-            border: '4px solid #fff',
-            zIndex: '5'
-          }}
-        >
+        {pageInfo.image && (
           <img
-            src={pageInfo.authorImage}
+            src={pageInfo.image}
             style={{
               width: '100%',
-              height: '100%'
-              // transform: 'scale(1.04)'
+              height: '100%',
+              objectFit: 'cover'
             }}
           />
-        </div>
-      )}
-    </div>,
+        )}
+      </div>
+    ),
     {
       width: 1200,
       height: 630,
